afe31a95e4d3f7d76d0a44e6a1afdcff4f278c9c
wyna <queralt.guillen@gmail.com>
2024-07-02 11:18:10 +0200

    updated looks for toolCards, searchbar background color

--- app/components/ToolCard.tsx
+++ app/components/ToolCard.tsx
@@ -6,7 +6,6 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-
 export interface ToolCardProps {
   tool: ToolCard;
 }
@@ -39,7 +38,7 @@ const ToolCardComponent = ({ tool}: ToolCardProps) => {
   };
 
   return (
-    <div className=" border-slate-50 border-4 p-4 rounded-xl shadow-md flex flex-col items-center m-4">
+    <div className=" border-slate-50 border-4 p-6 rounded-xl  shadow-slate-400 shadow-xl flex flex-col items-center m-4">
       <Link href={`/tools/${tool.id}`}>
         <div className="relative h-64 rounded-m overflow-hidden bg-cover w-80 bg-center " 
           style={{ backgroundImage: `url(${tool.picture || defaultImage})` }}>
@@ -49,9 +48,9 @@ const ToolCardComponent = ({ tool}: ToolCardProps) => {
         <div className="flex flex-col items-start">
           <p className="text-lg font-semibold">{tool.name}</p>
           <p className="text-gray-600">{tool.location}</p>
-          <h2 className="text-gray-600">{tool.ownerId}</h2>
+          <h2 className="text-gray-600">{tool.owner.name}</h2>
         </div>
-        <div className="flex flex-col items-center justify-between">
+        <div className="flex flex-col items-end justify-between">
           <h1 className="text-2xl font-semibold text-gray-900">${tool.dailyRate}</h1>
           <div className="relative text-[2rem] hover:cursor-pointer" onClick={handleLike}>{
             tool.liked ?
